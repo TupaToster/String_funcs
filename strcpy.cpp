3a46d64baed28973a6f64286efae24cfec6615e3
TupaToster <maxsid2004@mail.ru>
2022-08-28 23:45:31 +0300

    added my_smart_strcpy

--- strcpy.cpp
+++ strcpy.cpp
@@ -11,8 +11,6 @@
     A simple function that copies  "from" to "to" without minding the sizes of both
     Actually may cause segmentation fault if used wrong
 */
-
-
 action_status my_strcpy (char* from, char* to) {
 
     if (from == NULL or to == NULL) return MEM_ERR;
@@ -42,7 +40,6 @@ max amount of copied chars (with \0) is "n"
 
 Still may cause segmentation fault if used wrong but more rare
 */
-
 action_status my_strncpy (char* from, char* to, unsinged int n) {
 
     if (from == NULL or to == NULL) return MEM_ERR;
@@ -57,4 +54,43 @@ action_status my_strncpy (char* from, char* to, unsinged int n) {
     
     if(iter == n-1) return OVERFL;
     else return OK;
+}
+
+/*!
+    \brief The smartest copy paste of all three
+    \param from - c-string to copy from
+    \param to   - c-string to copy to
+    \return OK - if all ok and pc had enough memory
+    \return MEM_ERR - one of parameters is either NULL or points to NULL
+    \return OVERFL - if pc does not have enough memory to fully copy the string
+
+This thing also copies from "from" to "to" but with another twist - 
+it actually allocates right enough memory for all "from" to fit in "to".
+
+In case if this is impossible returns OVERFL and keeps the strings' contents
+*/
+action_status my_smart_strcpy (char** from, char** to) {
+
+    if (from  == NULL or
+        to    == NULL or
+        *from == NULL or
+        *to   == NULL) {
+            
+        return MEM_ERR;
+    }
+
+    char* temp = NULL ///< Temporary pointer to store allocated memory's adress
+
+    temp = malloc (*to, string_size (*from));
+    if(temp == NULL) return OVERFL;
+    else             *str = temp;
+
+    unsigned int iter = 0;
+    for (iter = 0; from[iter] != '\0'; iter++) {
+
+        *to[iter] = *from[iter];
+    }
+    *to[iter] = '\0';
+
+    return OK;
 }
