824c57b23c3958c41f6379f1ffe7bd88a02b4c63
TupaToster <maxsid2004@mail.ru>
2022-08-28 22:58:44 +0300

    quick fix by vscode : disable some error squiggles

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main (int argc, char* argv[]){
 	
 	if (argc > 1 and strcmp (argv[1], "--test") == 0){
 		unit_test ();
-		return 0;	 	
+		return 0; 	
 	}
 
 	printf ("Input string to apply stuff to: ");
@@ -14,7 +14,7 @@ int main (int argc, char* argv[]){
 	s = (char*) malloc (1);
 	if (s == NULL) return -1;	
 
-	if (my_getline (&s) == MEM_LIM) return -1;
+	if (my_getline (&s) == MEM_ERR) return -1;
 
 	printf("%s", s);
 }	
---
+++ strcpy.cpp
@@ -0,0 +1,14 @@
+#include "protos.h"
+
+/*!
+    \brief A simply strcpy function
+    \param char* from - string to copy from
+    \param char* to   - string to copy to
+    \return OK 
+*/
+
+
+action_status my_strcpy (char* from, char* to){
+
+    
+}
