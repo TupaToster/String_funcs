f49fde59b305465a6890161002825e89d9cf9f5e
TupaToster <maxsid2004@mail.ru>
2022-09-02 21:21:30 +0300

    Added dynamical resize of lines array

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "protos.h"
 
+
 /*!
     \brief Will contain some ways to interact with functions
     \param argc - count of com line args
@@ -22,30 +23,25 @@ int main (int argc, char* argv[]) {
     FILE* onegin = fopen ("onegin", "r"), 
         * out    = fopen ("out", "w");
 
-    char* lines[6852] = {NULL},
-        * temp        = (char*) calloc (201, 1);
+    char** lines = NULL;
+    char* temp = (char*) calloc (201, 1);
+    unsigned int line_amount = 0;
 
-    for (int i = 0; i < 6852; i++) {
+    while (fgets (temp, 200, onegin) != NULL) line_amount++;
 
-        lines[i] = (char*) calloc (201, 1);
-        lines[i] = fgets (lines[i], 200, onegin);
-        while (lines[i][0] == '\t') lines[i]++;
+    lines = (char**) calloc (line_amount, sizeof (char*));
+
+    fseek (onegin, SEEK_SET, 0);
+
+    for (unsigned int i = 0; i < line_amount; i++) {
+
+        assert (fgets (temp, 200, onegin) != NULL);
+        lines[i] = (char*) malloc (strlen(temp) + 1);
+        strcpy (lines[i], temp);
+        while (lines[i][0] == '\t' or lines[i][0] == ' ') lines[i]++;
     }
 
-    for (int i = 0; i < 6852; i++) {
-
-        for (int j = 1; j < 6852; j++) {
-
-            if (strcmp (lines[j], lines[j-1]) < 0) {
-
-                temp = lines[j];
-                lines[j] = lines[j-1];
-                lines[j-1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < 6852; i++) {
+    for (unsigned int i = 0; i < line_amount; i++) {
 
         fputs (lines[i], out);
     }
